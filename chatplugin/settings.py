f8bf92705718469242a9d69fa5fba2e91087120f
Your Name <you@example.com>
2024-04-29 16:48:27 +0300

    frontend interface

--- chatplugin/settings.py
+++ chatplugin/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'textbox/static'),
+    # os.path.join(BASE_DIR, 'your_app2/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- chatplugin/urls.py
+++ chatplugin/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('textbox/', include('textbox.urls')),
+    # path('', include('core.urls')),
+    path('', include('textbox.urls')),
 ]
